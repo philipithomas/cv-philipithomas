60d9af52975f9bee09b58873373c832513677bae
Philip I. Thomas <mail@philipithomas.com>
2012-10-17 18:19:23 -0500

    Formatting tweaks

--- index.php
+++ index.php
@@ -169,18 +169,18 @@ function position($company,$city,$job,$dates,$blob) {
 	
 	echo '
 		<div class="row">
-			<div class="span6" class="pull-left" style="font-weight: bold">'.$company.'</div>	
-			<div class="span6 class="pull-right" style="font-weight: bold">'.$city.'</div>
+			<div class="span6" style=""><h3>'.$company.'</h3></div>	
+			<div class="span6  style="font-weight: bold; text-align:right"><h3>'.$city.'</h3></div>
 		</div> <!-- end row-->
 			
 		<div class="row">
-			<div class="span6" class="pull-left" style="font-style:italic">'.$job.'</div>	
-			<div class="span6 class="pull-right" style="font-style:italic">'.$dates.'</div>
+			<div class="span6" style="font-style:italic">'.$job.'</div>	
+			<div class="span6  style="font-style:italic; text-align:right">'.$dates.'</div>
 		</div> <!-- end row-->
 		';
 		if (!empty($blob)) {
 			echo '
-			<div class="row">'.$blob.'</div>';
+			<div class="row"><div class="span12">'.$blob.'</div></div>';
 			}	
 			
 }
