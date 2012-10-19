e5ee89956c5bd1a6360933fdf5cca34a7b0dbec6
Philip I. Thomas <mail@philipithomas.com>
2012-10-19 17:41:35 -0500

    Nevermind, just adding analytics

--- index.php
+++ index.php
@@ -24,7 +24,20 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="../assets/ico/favicon.ico">
+	<script type="text/javascript">
 
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-9939917-3']);
+	  _gaq.push(['_setDomainName', 'philipithomas.com']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
   
   </head>
 
